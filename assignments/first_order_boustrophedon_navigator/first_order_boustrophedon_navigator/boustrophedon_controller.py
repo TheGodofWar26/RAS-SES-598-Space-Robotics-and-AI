db1206926f2c3fa53ad00ae1c019f7f7d6c04e17
TheGodofWar26 <spakhar1@asu.edu>
2025-02-20 12:04:26 -0700

    checking before pull

--- assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
+++ assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
@@ -19,11 +19,11 @@ class BoustrophedonController(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('Kp_linear', 10.0),
-                ('Kd_linear', 0.1),
-                ('Kp_angular', 5.0),
-                ('Kd_angular', 0.2),
-                ('spacing', 1.0)
+                ('Kp_linear', 19),
+                ('Kd_linear', 0.4),
+                ('Kp_angular', 12),
+                ('Kd_angular', 0.01),
+                ('spacing', 1.2)
             ]
         )
         
